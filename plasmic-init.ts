a01d212b375bd26db9cca19473ccff5f3edbbe7b
Gaël <prigentgael@gmail.com>
2025-02-12 07:42:22 +0100

    Fix project id and token env

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -3,8 +3,8 @@ import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
 export const PLASMIC = initPlasmicLoader({
   projects: [
     {
-      id: process.env.NEXT_PUBLIC_PLASMIC_PROJECT_ID,
-      token: process.env.NEXT_PUBLIC_PLASMIC_PROJECT_TOKEN,
+      id: process.env.NEXT_PUBLIC_PLASMIC_PROJECT_ID!,
+      token: process.env.NEXT_PUBLIC_PLASMIC_PROJECT_TOKEN!,
     },
   ],
 
